e1acdb2a5193cd62429225a10a81fbef0debf4c5
legend <legendprosper2125@gmail.com>
2025-08-18 05:12:36 +0100

    updated the portfolio

--- src/components/Portfolio/data.jsx
+++ src/components/Portfolio/data.jsx
@@ -1,50 +1,82 @@
 import portfolioImage from "../../assets/portfolioImage.jpg"
 import portfolioImage1 from "../../assets/portfolioImage1.jpg"
+import bannerly from "../../assets/bannerly.jpg"
+import flexvest from "../../assets/flexvest.jpg"
+import afrovivo from "../../assets/afrovivo.jpg"
+import readcode from "../../assets/readcode.jpg"
 
 const recentWorks = [
     {
         alter: "projects",
         div1: "portfolio-info",
-        h3: "coming soon...",
-        portfolioImage: portfolioImage,
-    },
-    {
-        alter: "projects",
-        div1: "portfolio-info",
-        portfolioImage: portfolioImage,
-        h3: "coming soon..."
-    },
-    {
-        alter: "projects",
-        div1: "portfolio-info",
-        portfolioImage: portfolioImage,
-        h3: "coming soon..."
-    },
-    {
-        alter: "projects",
-        div1: "portfolio-info",
-        portfolioImage: portfolioImage,
-        h3: "coming soon..."
-    },
-    {
-        alter: "projects",
-        portfolioImage: portfolioImage,
-        div1: "portfolio-info",
-        h3: "coming soon..."
-    },
-    {
-        alter: "projects",
-        div1: "portfolio-info",
-        h3: "shapetrade",
-        p: "Lorem ipsum dolorportfolioImage1 sit amet consectetur adipisicing elit. Nisi, porro.",
+        h3: "BANNERLY",
+        p: "An AI system that analyzes thousands of professional banners to help you create designs that stand out while maintaining your personal brand identity.",
         div2: "portfolio-link",
-        anchor: "afrovivo-waitlist-coral.vercel.app",
+        anchor: "https://bannerly.vercel.app",
         anchor1Id: "link-icon2",
         linkI: "fa-solid fa-arrow-up-right-from-square",
         anchor2Id: "link-icon2",
         gitL: "fa-brands fa-github",
-        portfolioImage: portfolioImage1,
+        portfolioImage: bannerly,
     },
+    {
+        alter: "projects",
+        div1: "portfolio-info",
+        h3: "FlexVest",
+        p: "FlexVest helps Nigerians protect their money from Naira inflation by saving in USDC on Solana — simple, secure, and accessible.",
+        div2: "portfolio-link",
+        anchor: "https://flex-vest-main-npsy.vercel.app/",
+        anchor1Id: "link-icon2",
+        linkI: "fa-solid fa-arrow-up-right-from-square",
+        anchor2Id: "link-icon2",
+        gitL: "fa-brands fa-github",
+        portfolioImage: flexvest,
+    },
+    {
+        alter: "projects",
+        div1: "portfolio-info",
+        h3: "FlexVest",
+        p: "A platform where you connect with clients worldwide who are looking for your unique skills and talents.",
+        div2: "portfolio-link",
+        anchor: "https://afrovivo-waitlist-coral.vercel.app/",
+        anchor1Id: "link-icon2",
+        linkI: "fa-solid fa-arrow-up-right-from-square",
+        anchor2Id: "link-icon2",
+        gitL: "fa-brands fa-github",
+        portfolioImage: afrovivo,
+    },
+    {
+        alter: "projects",
+        div1: "portfolio-info",
+        h3: "FlexVest",
+        p: "Get access to programming e-books for free.",
+        div2: "portfolio-link",
+        anchor: "https://read-code.netlify.app//",
+        anchor1Id: "link-icon2",
+        linkI: "fa-solid fa-arrow-up-right-from-square",
+        anchor2Id: "link-icon2",
+        gitL: "fa-brands fa-github",
+        portfolioImage: readcode,
+    },
+    // {
+    //     alter: "projects",
+    //     portfolioImage: portfolioImage,
+    //     div1: "portfolio-info",
+    //     h3: "coming soon..."
+    // },
+    // {
+    //     alter: "projects",
+    //     div1: "portfolio-info",
+    //     h3: "shapetrade",
+    //     p: "Lorem ipsum dolorportfolioImage1 sit amet consectetur adipisicing elit. Nisi, porro.",
+    //     div2: "portfolio-link",
+    //     anchor: "afrovivo-waitlist-coral.vercel.app",
+    //     anchor1Id: "link-icon2",
+    //     linkI: "fa-solid fa-arrow-up-right-from-square",
+    //     anchor2Id: "link-icon2",
+    //     gitL: "fa-brands fa-github",
+    //     portfolioImage: portfolioImage1,
+    // },
 ]
 
 export default recentWorks
